e479c152c9ce2dbf4f3837cfd99d9f50fb851bae
Muhammad Resna Rizki Pratama <resnarizki29@gmail.com>
2014-04-03 18:00:33 +0700

    Fix typo in if statement

--- app/views/pages/ifgames/anggota/index.blade.php
+++ app/views/pages/ifgames/anggota/index.blade.php
@@ -217,12 +217,11 @@
                 Button::lg_primary_link_block(URL::route('ifgames.anggota.download'), Helper::fa('download').' Download kuitansi')
             }}
             @else
-                @if($tim->anggota_lengkap()
+                @if($tim->anggota_lengkap())
                 {{ Alert::warning('Download kuitansi belum aktif. Dokumen persyaratan belum diverifikasi.') }}
                 @else
                 {{ Alert::warning('Silahkan lengkapi anggota tim dan melengkapi dokumen persyaratan untuk mendownload kuitansi.') }}
                 @endif
-            
             @endif
         </div>
     </div>
