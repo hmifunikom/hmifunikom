34c32f15463af1dcafc06458e7542b54e873fe10
Muhammad Resna Rizki Pratama <resnarizki29@gmail.com>
2014-08-10 18:16:03 +0700

    Add cakrawala email templates

---
+++ app/views/emails/cakrawala/prosedur-pembayaran-tcr.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <title>[CAKRAWALA] Prosedur Pembayaran - HMIF Unikom</title>
+    </head>
+    <body>
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="http://cakrawala.hmifunikom.org">Cakrawala HMIF Unikom</a>
+                </div>
+            </div>
+        </div>
+        <div class="big-container">
+            <div class="container">
+                <br>
+                <div>Anda baru saja berhasil mendaftarkan akun di cakrawala The Color Run. Selanjutnya silahkan untuk melakukan prosedur pembayaran sebagai berikut:</div>
+                <div class="well well-lg">
+                    <ol>
+                        <li>Silahkan mentransfer biaya pendaftaran (Rp 40.000) ke Rekening <strong>BNI 0268421843 a.n. Melindah.</strong>
+                        </li><li>Foto atau scan hasil bukti transfer.
+                        </li><li>Login kembali ke situs Cakrawala.
+                        <li>Upload foto bukti transfer untuk verifikasi pembayaran.
+                        </li>
+                    </ol>
+                </div>
+                <br><br>
+                <span>HMIF Unikom</span>
+            </div>
+        </div>
+    </body>
+</html>
