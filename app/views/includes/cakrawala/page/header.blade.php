eeb565521971df717e6e114768069513a3e1ebff
Muhammad Resna Rizki Pratama <resnarizki29@gmail.com>
2014-08-11 06:40:33 +0700

    Fix cakrawala menu peserta

--- app/views/includes/cakrawala/page/header.blade.php
+++ app/views/includes/cakrawala/page/header.blade.php
@@ -18,10 +18,14 @@
                 <li>
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::cakrawala()->user()->username }} {{Helper::fa('angle-down')}}<div class="clearfix"></div></a>
                     <ul class="dropdown-menu">
+                        @if(Auth::cakrawala()->user()->userable instanceof HMIF\Model\Cakrawala\Tim)
                         <li {{ Helper::active('cakrawala.anggota.index') }}><a href="{{action('cakrawala.anggota.index')}}">{{Helper::fa(
                             'group')}} Form Anggota</a></li>
                         <li {{ Helper::active('cakrawala.persyaratan.index') }}><a href="{{action('cakrawala.persyaratan.index')}}">{{Helper::fa('files-o')}} Form Persyaratan</a></li>
                         <li {{ Helper::active('cakrawala.karya.index') }}><a href="{{action('cakrawala.karya.index')}}">{{Helper::fa('star')}} Form Karya</a></li>
+                        <li class="divider"></li>
+                        @endif
+                        <li {{ Helper::active('cakrawala.pembayaran.edit') }}><a href="{{action('cakrawala.pembayaran.edit')}}">{{Helper::fa('money')}} Status Pembayaran</a></li>
                         <li class="divider"></li>
                         <li><a href="{{action('cakrawala.sessions.destroy')}}">{{Helper::fa('sign-out')}} Keluar</a></li>
                     </ul>
